07252b71518e60cf29850ac7212e6269cddb543f
flowers-10 <mgaligigi@163.com>
2023-05-04 23:04:44 +0800

    feat(Stack):Common method implementation of stack structure

--- code/Stack/01_实现栈结构Stack.ts
+++ code/Stack/01_实现栈结构Stack.ts
@@ -1,13 +1,48 @@
 // 封装Stack
 class ArrayStack {
   // 定义一个数组，用于存储元素
-  private data:any[] = []
+  private data: any[] = [];
 
   // 实现栈操作方法
-  push() {
 
+  // 元素压入栈
+  push(element: any): void {
+    this.data.push(element);
+  }
+
+  // 将栈顶元素弹出栈
+  pop(): any {
+    return this.data.pop();
+  }
+
+  // 查看栈顶元素
+  peek(): any {
+    return this.data[this.data.length - 1];
+  }
+
+  // 判断栈是否为空
+  isEmpty():boolean {
+    return this.data.length === 0;
+  }
+
+  // 返回栈的数据个数
+  size():number {
+    return this.data.length;
   }
 }
 
 // 创建Stack实例
-const stack1 = new ArrayStack()
+const stack1 = new ArrayStack();
+
+stack1.push("aaa")
+stack1.push("bbb")
+stack1.push("ccc")
+
+console.log(stack1.peek());
+console.log(stack1.pop());
+console.log(stack1.pop());
+console.log(stack1.pop());
+
+console.log(stack1.isEmpty());
+console.log(stack1.size());
+
