dac4900239db643d89d3e9c0a6f461afec320921
flowers-10 <mgaligigi@163.com>
2023-05-05 21:46:42 +0800

    refactor(Stack):stack reconstruction

--- code/Stack/Stack.ts
+++ code/Stack/Stack.ts
@@ -1,42 +1,42 @@
 // 封装Stack
-class ArrayStack {
+class ArrayStack<T = any> {
   // 定义一个数组，用于存储元素
-  private data: any[] = [];
+  private data: T[] = [];
 
   // 实现栈操作方法
 
   // 元素压入栈
-  push(element: any): void {
+  push(element: T): void {
     this.data.push(element);
   }
 
   // 将栈顶元素弹出栈
-  pop(): any {
+  pop(): T | undefined {
     return this.data.pop();
   }
 
   // 查看栈顶元素
-  peek(): any {
+  peek(): T | undefined {
     return this.data[this.data.length - 1];
   }
 
   // 判断栈是否为空
-  isEmpty():boolean {
+  isEmpty(): boolean {
     return this.data.length === 0;
   }
 
   // 返回栈的数据个数
-  size():number {
+  size(): number {
     return this.data.length;
   }
 }
 
 // 创建Stack实例
-const stack1 = new ArrayStack();
+const stack1 = new ArrayStack<string>();
 
-stack1.push("aaa")
-stack1.push("bbb")
-stack1.push("ccc")
+stack1.push("aaa");
+stack1.push("bbb");
+stack1.push("ccc");
 
 console.log(stack1.peek());
 console.log(stack1.pop());
@@ -46,4 +46,9 @@ console.log(stack1.pop());
 console.log(stack1.isEmpty());
 console.log(stack1.size());
 
-export {}
+const stack2 = new ArrayStack<number>();
+stack2.push(1);
+stack2.push(6);
+stack2.push(10);
+
+export {};
