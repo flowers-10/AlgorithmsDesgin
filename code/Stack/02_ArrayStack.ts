95d73d318ab520c93f88637f30af59418b333fc0
flowers-10 <mgaligigi@163.com>
2023-05-05 22:09:24 +0800

    test(Stack-question):decimal to binary

--- code/Stack/02_ArrayStack.ts
+++ code/Stack/02_ArrayStack.ts
@@ -33,24 +33,4 @@ class ArrayStack<T = any> implements IStack<T> {
   }
 }
 
-// 创建Stack实例
-const stack1 = new ArrayStack<string>();
-
-stack1.push("aaa");
-stack1.push("bbb");
-stack1.push("ccc");
-
-console.log(stack1.peek());
-console.log(stack1.pop());
-console.log(stack1.pop());
-console.log(stack1.pop());
-
-console.log(stack1.isEmpty());
-console.log(stack1.size());
-
-const stack2 = new ArrayStack<number>();
-stack2.push(1);
-stack2.push(6);
-stack2.push(10);
-
-export {};
+export default ArrayStack;
---
+++ code/Stack/04_十进制转二进制.ts
@@ -0,0 +1,26 @@
+import ArrayStack from "./02_ArrayStack";
+
+function decimalToBinary(decimal: number): string {
+  // 1.创建一个栈，用于存放余数
+  const stack = new ArrayStack<number>();
+
+  // 2.循环：
+  while (decimal > 0) {
+    const result = decimal % 2;
+    stack.push(result);
+
+    decimal = Math.floor(decimal / 2);
+  }
+  // 3.所有余数已经存入stack
+  let binary = "";
+  while (!stack.isEmpty()) {
+    binary += stack.pop();
+  }
+  return binary;
+}
+
+console.log(decimalToBinary(35));
+console.log("--------");
+console.log(decimalToBinary(100));
+
+export {};
