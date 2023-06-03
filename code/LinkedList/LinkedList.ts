bba4743fad19f0a84d6e5c1966101635c3fbdf11
flowers-10 <mgaligigi@163.com>
2023-06-03 22:58:06 +0800

    feat(LinkedList):add other  method

--- code/LinkedList/LinkedList.ts
+++ code/LinkedList/LinkedList.ts
@@ -17,15 +17,14 @@ class LinkedList<T> {
   }
   // 封装私有方法
   // 根绝positon获取当前的节点（不是节点的value，而是获取节点)
-  private getNode(position:number) : Node<T> | null {
-    let index = 0
-    let current = this.head
-    while (index ++ < position && current) {
-      current = current.next
+  private getNode(position: number): Node<T> | null {
+    let index = 0;
+    let current = this.head;
+    while (index++ < position && current) {
+      current = current.next;
     }
-    return current
+    return current;
   }
-
 
   // 追加节点
   append(value: T) {
@@ -71,8 +70,8 @@ class LinkedList<T> {
       newNode.next = this.head;
       this.head = newNode;
     } else {
-      const previous = this.getNode(position - 1)
-      
+      const previous = this.getNode(position - 1);
+
       newNode.next = previous!.next;
       previous!.next = newNode;
     }
@@ -84,13 +83,13 @@ class LinkedList<T> {
   //链表插入元素的方法
   removeAt(position: number): T | null {
     // 1.越界判断
-    if (position < 0 || position >= this.size)  return null
+    if (position < 0 || position >= this.size) return null;
 
-    let current = this.head
+    let current = this.head;
     if (position === 0) {
       this.head = current?.next ?? null;
     } else {
-      const previous = this.getNode(position - 1)
+      const previous = this.getNode(position - 1);
       previous!.next = previous?.next?.next ?? null;
     }
     this.size--;
@@ -98,10 +97,42 @@ class LinkedList<T> {
     return current?.value ?? null;
   }
 
+  // 获取方法
   get(position: number): T | null {
     if (position < 0 || position >= this.size) return null;
-   
-    return this.getNode(position)?.value ?? null
+
+    return this.getNode(position)?.value ?? null;
+  }
+  // 更新方法
+  update(value: T, position: number): boolean {
+    if (position < 0 || position >= this.size) return false;
+    const currentNode = this.getNode(position);
+    currentNode!.value = value;
+    return true;
+  }
+  // 根据值获取该值位置索引
+  indexOf(value: T): number {
+    let current = this.head;
+    let index = 0;
+    while (current) {
+      if (current.value === value) {
+        return index;
+      }
+      index++;
+      current = current.next;
+    }
+    return -1;
+  }
+
+  // 删除方法，根据value删除
+  remove(value: T): T | null {
+    const index = this.indexOf(value);
+    return this.removeAt(index);
+  }
+
+  // 判断单链表是否为空的方法
+  isEmpty() {
+    return this.size === 0;
   }
 }
 
@@ -114,14 +145,33 @@ linkedList.append("ccc");
 linkedList.append("ddd");
 linkedList.insert("666", 5);
 
-linkedList.traverse();
-linkedList.removeAt(2)
-linkedList.removeAt(2)
-linkedList.traverse();
+// linkedList.traverse();
+// linkedList.removeAt(2)
+// linkedList.removeAt(2)
+// linkedList.traverse();
 
 // console.log(linkedList.get(0));
 // console.log(linkedList.get(1));
 // console.log(linkedList.get(2));
 
+// 测试update
+linkedList.traverse();
+linkedList.update("迈尔斯", 2);
+linkedList.update("格温", 3);
+linkedList.traverse();
+
+// 测试indexof
+console.log(linkedList.indexOf("迈尔斯"));
+console.log(linkedList.indexOf("格温"));
+
+// 测试remove
+linkedList.remove('迈尔斯')
+linkedList.remove('格温')
+linkedList.remove('aaa')
+linkedList.remove('bbb')
+linkedList.remove('ddd')
+
+
+console.log(linkedList.isEmpty());
 
 export {};
