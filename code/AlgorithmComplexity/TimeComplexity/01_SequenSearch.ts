cf8c68524bd48c9dbdce787e2d696bb0231cb31f
flowers-10 <mgaligigi@163.com>
2023-06-24 19:53:40 +0800

    feat(AlgorithmComplexity):时间复杂度

---
+++ code/AlgorithmComplexity/TimeComplexity/01_SequenSearch.ts
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @param array 查找的数组
+ * @param target 查找的目标元素
+ * @returns 查找到的索引，没有返回-1
+ */
+
+function sequenSearch(array:number[],target:number) {
+  let result = -1
+  for (let i = 0;i<array.length;i++) {
+    array[i] === target ? result = i : undefined
+  }
+  return result
+}
+
+console.log(sequenSearch([1,2,3,4,5,6,7,8,9,10],1));
