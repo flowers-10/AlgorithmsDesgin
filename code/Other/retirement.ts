991383154825e5b1914189b5f3c952a4041e5e68
Michela <mgaligigi@163.com>
2023-08-28 10:49:32 +0800

    refactor(Other):退休金额计算

--- code/Other/retirement.ts
+++ code/Other/retirement.ts
@@ -1,29 +1,30 @@
-let amount = 60000;
-
-let inflationAmount = 60000;
-let age = 27;
 type ResArrType = {
-  age:number,
-  inflationAmount:number,
-  resAmount?:number
-}
-let resArr:ResArrType[] = [{ age: 27, inflationAmount: 60000 }];
+  age: number;
+  inflationAmount: number;
+  resAmount?: number;
+};
+const retirement = (inflationAmount: number, targetAge: number) => {
+  let resArr: ResArrType[] = [{ age:targetAge, inflationAmount}];
+  let age = targetAge;
+  for (let i = 0; i < (65 - targetAge); i++) {
+    inflationAmount *= 1.027;
+    age++;
+    resArr.push({
+      age,
+      inflationAmount,
+    });
+  }
 
-let tt = [];
-
-for (let i = 0; i < 65 - 27; i++) {
-  inflationAmount *= 1.027;
-  age++;
-  resArr.push({
-    age,
-    inflationAmount,
-  });
-}
-
-resArr.forEach((item,index) => {
+  resArr.forEach((item, index) => {
     let resAmount = 0;
-    for( let i = index;i<resArr.length;i++) {
-        resAmount += resArr[i].inflationAmount; 
+    for (let i = index; i < resArr.length - 1; i++) {
+      resAmount += resArr[i].inflationAmount;
     }
     item.resAmount = resAmount;
-})
+  });
+
+  return resArr
+};
+
+console.log(retirement(60000, 27));
+
